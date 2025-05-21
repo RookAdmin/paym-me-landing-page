144343d079a5f6f8113020191247f1617cf2dfb4
Karthik Kishore B <karthikkishore2603@gmail.com>
2025-05-21 22:44:33 +0530

    logo change

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ const Index = () => {
         {/* Logo */}
         <div className="mb-12 md:mb-16">
           <img 
-            src="/lovable-uploads/461b1bd9-e48a-43f9-85c6-7879690b0a77.png" 
+            src="/lovable-uploads/logo.png" 
             alt="Paym.me by Rook" 
             className="h-16 md:h-20" 
           />
@@ -25,18 +25,20 @@ const Index = () => {
             Experience fast, secure transactions designed with your needs in mind.
           </p>
           
-          {/* Waitlist Form */}
-          <div className="w-full flex justify-center mb-8">
-            <WaitlistForm />
-          </div>
-          
-          {/* Call to action */}
           <div className="flex flex-col items-center mt-8 animate-bounce">
             <p className="text-sm text-gray-500 mb-2">
               Limited spots available
             </p>
             <ArrowDown className="h-4 w-4 text-rook-red" />
           </div>
+
+          {/* Waitlist Form */}
+          <div className="w-full flex justify-center mb-8">
+            <WaitlistForm />
+          </div>
+          
+          {/* Call to action */}
+          
         </div>
       </div>
       
